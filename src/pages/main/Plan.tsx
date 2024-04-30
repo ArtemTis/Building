89f9d60dd82175dbb95d18a0a9079eb786305090
Artem Tishenko <artem.200207@yandex.ru>
2024-05-01 00:47:17 +0400

    finish desctop sections

--- src/pages/main/Plan.tsx
+++ src/pages/main/Plan.tsx
@@ -8,7 +8,44 @@ const Plan = () => {
             </h2>
 
             <div className="plan-container">
+                <div className="plan-step">
+                    <h2>1</h2>
+                    <h4>Cогласовывание</h4>
+                </div>
 
+                <div className="plan-step">
+                    <h2>2</h2>
+                    <h4>Схема планировки</h4>
+                </div>
+
+                <div className="plan-step">
+                    <h2>3</h2>
+                    <h4>Заливка фундамента</h4>
+                </div>
+
+                <div className="plan-step2">
+                    <h2>4</h2>
+                    <h4>Возведение стен</h4>
+                </div>
+
+                <div className="plan-step2">
+                    <div className="plan-step-content">
+
+                        <div className='plan-step-title'>
+                            <h2>5</h2>
+                            <h4>Установка крыши</h4>
+                        </div>
+
+
+                        <button className='project-discuss'>Обсудить проект дома</button>
+                    </div>
+
+                </div>
+
+                <div className="plan-step2">
+                    <h2>6</h2>
+                    <h4>Внешняя отделка</h4>
+                </div>
             </div>
 
         </div>
